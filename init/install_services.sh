f1ce209b4d91d25e100fd87e1d50f8f2496c32ff
Kai Kreuzer <kai.kreuzer@telekom.de>
2024-10-02 15:10:27 +0200

    Simplify and automate demo setup
    - heavily reduced the steps a user has to do for getting a running system by automating the initial setup
    - removed unused files
    - renamed channel to "web" for tenant acme
    - removed canary channel demo with the idea to make this one of many demos that can be built upon the initial setup
    - switched model from GPT3.5 to GPT4o-mini, since 3.5 is considered deprecated by OpenAI

---
+++ init/install_services.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2024 Deutsche Telekom AG
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Exit immediately if a command exits with a non-zero status
+#set -e
+
+source .env
+
+# Install Istio with the default profile and debug logging level
+istioctl operator init
+kubectl apply -f istio/istio-operator.yaml
+
+#istioctl install --set profile=demo -y
+kubectl apply -f ${ISTIO_HOME}/samples/addons/prometheus.yaml
+kubectl apply -f ${ISTIO_HOME}/samples/addons/grafana.yaml
+kubectl apply -f ${ISTIO_HOME}/samples/addons/jaeger.yaml
+kubectl apply -f ${ISTIO_HOME}/samples/addons/kiali.yaml
+
+# Label the default namespace for Istio sidecar injection
+kubectl label namespace default istio-injection=enabled --overwrite
+
+# Install lmos-operator chart
+helm upgrade --install lmos-operator oci://ghcr.io/lmos-ai/lmos-operator-chart \
+ --version 0.0.4-SNAPSHOT
+
+# Create Kubernetes openai secret for lmos-runtime
+kubectl delete secret lmos-runtime 2> /dev/null
+kubectl create secret generic lmos-runtime --from-literal=OPENAI_API_KEY="$OPENAI_API_KEY"
+kubectl apply -f istio/openai-external-service-entry.yaml
+
+# Install lmos-runtime chart
+helm upgrade --install lmos-runtime oci://ghcr.io/lmos-ai/lmos-runtime-chart \
+ --version 0.0.8-SNAPSHOT \
+ --set openaiApiUrl="$OPENAI_API_URL" \
+ --set openaiApiModel=GPT4o-mini \
+ --set agentRegistryUrl=http://lmos-operator.default.svc.cluster.local:8080
+
+# Wait for CRD to be created before installing agents
+echo "Waiting for LMOS agent CRD to be created..."
+while ! kubectl get crd agents.lmos.ai >/dev/null 2>/dev/null; do sleep 1; done
+echo "LMOS agent CRD created."
+
+# Install agents
+kubectl delete secret openai-secrets 2>/dev/null
+kubectl create secret generic openai-secrets --from-env-file=arc_config.env
+
+helm upgrade --install weather-agent oci://ghcr.io/lmos-ai/arc-weather-agent-chart --version 1.0.6
+helm upgrade --install news-agent oci://ghcr.io/lmos-ai/arc-news-agent-chart --version 1.0.6
+
+# Wait for pods to be running
+echo "Waiting for pods to be running..."
+while ! kubectl get pods -n istio-system | grep kiali | grep -q Running; do sleep 1; done
+while ! kubectl get pods -n istio-system | grep grafana | grep -q Running; do sleep 1; done
+while ! kubectl get pods -n istio-system | grep prometheus | grep -q Running; do sleep 1; done
+while ! kubectl get pods -n istio-system | grep jaeger | grep -q Running; do sleep 1; done
+while ! kubectl get pods | grep lmos-runtime | grep -q Running; do sleep 1; done
+
+# Set up port forwarding
+echo "Setting up port forwarding..."
+nohup kubectl argo rollouts dashboard >/dev/null 2>&1 &
+nohup kubectl -n istio-system port-forward svc/kiali 20001:20001 >/dev/null 2>&1 &
+nohup kubectl -n istio-system port-forward svc/grafana 3000:3000 >/dev/null 2>&1 &
+nohup kubectl -n istio-system port-forward svc/prometheus 9090:9090 >/dev/null 2>&1 &
+nohup kubectl -n istio-system port-forward svc/tracing 9411:80 >/dev/null 2>&1 &
+nohup kubectl port-forward svc/lmos-runtime 8081:8081 >/dev/null 2>&1 &
+
+echo "Setting up channel..."
+# Stable Channel – Includes only the weather agent
+kubectl apply -f init/acme-web-stable-channel.yml
