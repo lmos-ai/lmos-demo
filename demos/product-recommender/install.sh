dcce686c4092d054d8c3f8c42bc923a8f69885f2
Kai Kreuzer <kai.kreuzer@telekom.de>
2024-11-06 16:57:01 +0100

    Add google secrets to k8s for productsearch agent. (#8)

--- demos/product-recommender/install.sh
+++ demos/product-recommender/install.sh
@@ -10,7 +10,10 @@
 SCRIPT_DIR=$(dirname "$0")
 source "$SCRIPT_DIR/.env"
 
-helm upgrade --install productsearch-agent oci://ghcr.io/lmos-ai/productsearch-agent-chart --version 0.1.0-snapshot
-helm upgrade --install techspec-agent oci://ghcr.io/lmos-ai/techspec-agent-chart --version 0.1.0-snapshot
+# Update openai-secrets through kubectl by adding the .env variables to the existing secrets
+kubectl create secret generic openai-secrets --from-env-file="$SCRIPT_DIR/.env" --dry-run=client -o yaml | kubectl apply -f -
 
+helm upgrade --install productsearch-agent oci://ghcr.io/lmos-ai/productsearch-agent-chart --version 0.1.0-SNAPSHOT
+helm upgrade --install techspec-agent oci://ghcr.io/lmos-ai/techspec-agent-chart --version 0.1.0-SNAPSHOT
+ 
 kubectl apply -f "$SCRIPT_DIR/product-recommender-channel.yml"
